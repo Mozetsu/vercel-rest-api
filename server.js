e13495697a072a2407e04355f46b1b7acad0d31a
Mozetsu <hello@mozetsu.dev>
2022-03-14 19:00:56 +0000

    Add cors origin to io server

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const http = require('http');
-const cors = require('cors');
 const { Server } = require('socket.io');
 
 const PORT = process.env.PORT || 4000;
 const app = express();
-app.use(cors());
 app.get('/', (req, res) => res.json({ status: 'Online' }));
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: '*' } });
 
 io.on('connection', (socket) => {
 	console.log(`User Connected: ${socket.id}`);
