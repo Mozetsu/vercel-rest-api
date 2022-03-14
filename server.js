7f7dbcaaa4c751128f1e5c75df4ed0596ba8fbac
Mozetsu <hello@mozetsu.dev>
2022-03-14 17:30:48 +0000

    Add env variable

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
 
+const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
 app.get('/', (req, res) => res.json({ status: 'Online' }));
 
 const server = http.createServer(app);
-
 const io = new Server(server);
 
 io.on('connection', (socket) => {
@@ -17,4 +17,4 @@ io.on('connection', (socket) => {
 	socket.on('disconnect', () => console.log('User Disconnected', socket.id));
 });
 
-server.listen(3001, () => console.log('SERVER RUNNING'));
+server.listen(PORT, () => console.log('SERVER RUNNING'));
