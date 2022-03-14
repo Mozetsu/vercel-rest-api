7ad342aa69ace2edb7c81423e40211b8189199bb
Mozetsu <hello@mozetsu.dev>
2022-03-14 17:01:34 +0000

    Refactor index.js

--- index.js
+++ index.js
@@ -3,27 +3,18 @@ const app = express();
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
+
 app.use(cors());
+app.get('/', (req, res) => res.json({ status: 'Online' }));
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-	cors: {
-		origin: '*',
-		methods: ['GET', 'POST'],
-	},
-});
+const io = new Server(server);
 
 io.on('connection', (socket) => {
 	console.log(`User Connected: ${socket.id}`);
-
 	socket.emit('connected', socket.id);
-
-	socket.on('disconnect', () => {
-		console.log('User Disconnected', socket.id);
-	});
+	socket.on('disconnect', () => console.log('User Disconnected', socket.id));
 });
 
-server.listen(3001, () => {
-	console.log('SERVER RUNNING');
-});
+server.listen(3001, () => console.log('SERVER RUNNING'));
